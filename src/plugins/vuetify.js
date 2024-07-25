4e11e97bad126d17816445742263eb6c842e415e
Оськина Ксения Юрьевна <koskina@nic.etu>
2024-07-25 16:57:51 +0300

    project on vite

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,15 +1,19 @@
+/**
+ * plugins/vuetify.js
+ *
+ * Framework documentation: https://vuetifyjs.com`
+ */
+
 // Styles
 import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
 
-// Vuetify
+// Composables
 import { createVuetify } from 'vuetify'
-import { VNumberInput } from 'vuetify/labs/VNumberInput'
 
+// https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
-  components: {
-    VNumberInput,
-  }
-}
-  // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
-)
+  theme: {
+    defaultTheme: 'dark',
+  },
+})
--- src/router/index.js
+++ src/router/index.js
@@ -1,32 +1,36 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import AddCheque from '../views/AddChequeView.vue'
-import AddPeople from '../views/AddPeopleView.vue'
-import Results from '../views/ResultsView.vue'
-import Home from '../views/Home.vue'
+/**
+ * router/index.ts
+ *
+ * Automatic routes for `./src/pages/*.vue`
+ */
 
-
-const routes = [
-  {
-    path: '/cheque',
-    component: AddCheque
-  },
-  {
-    path: '/people',
-    component: AddPeople
-  },
-  {
-    path: '/results',
-    component: Results
-  },
-  {
-    path: '',
-    component: Home
-  }
-]
+// Composables
+import { createRouter, createWebHistory } from 'vue-router/auto'
+import { setupLayouts } from 'virtual:generated-layouts'
+import { routes } from 'vue-router/auto-routes'
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: setupLayouts(routes),
 })
 
-export default router;
+// Workaround for https://github.com/vitejs/vite/issues/11804
+router.onError((err, to) => {
+  if (err?.message?.includes?.('Failed to fetch dynamically imported module')) {
+    if (!localStorage.getItem('vuetify:dynamic-reload')) {
+      console.log('Reloading page to fix dynamic import error')
+      localStorage.setItem('vuetify:dynamic-reload', 'true')
+      location.assign(to.fullPath)
+    } else {
+      console.error('Dynamic import error, reloading page did not fix it', err)
+    }
+  } else {
+    console.error(err)
+  }
+})
+
+router.isReady().then(() => {
+  localStorage.removeItem('vuetify:dynamic-reload')
+})
+
+export default router
